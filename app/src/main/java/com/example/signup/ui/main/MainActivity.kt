9dd2604fc3f5510fab02d1f70819b7b3a2cface6
gihop <giho-p@hanmail.net>
2020-09-22 19:58:33 +0900

    SignUp layout 생성.

--- app/src/main/java/com/example/signup/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/signup/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.signup
+package com.example.signup.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.signup.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
