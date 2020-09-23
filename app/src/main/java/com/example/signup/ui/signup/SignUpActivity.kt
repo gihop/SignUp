5c43610d6c86646f6234e342c81c74e3922679cf
gihop <giho-p@hanmail.net>
2020-09-23 20:17:17 +0900

    유효성 검사가 끝나면 제출 버튼 작동.

--- app/src/main/java/com/example/signup/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/signup/ui/signup/SignUpActivity.kt
@@ -5,8 +5,8 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.signup.R
+import com.jakewharton.rxbinding4.view.clicks
 import com.jakewharton.rxbinding4.widget.textChangeEvents
-import com.jakewharton.rxbinding4.widget.textChanges
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_signup.*
@@ -22,25 +22,85 @@ class SignUpActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
+        submit_button.isEnabled = false
+
         disposables.add(email_edit.textChangeEvents()
             .map{ it.text }
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe{ text ->
                 validate(text.toString(), Type.EMAIL)
             })
+
+        disposables.add(password_edit.textChangeEvents()
+            .map{ it.text }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe{ text ->
+                validate(text.toString(), Type.PASSWORD)
+            })
+
+        disposables.add(password_confirm_edit.textChangeEvents()
+            .map{ it.text }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe{ text ->
+                validate(text.toString(), Type.CONFIRM)
+            })
+
+        disposables.add(nickname_edit.textChangeEvents()
+            .map{ it.text }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe{ text ->
+                validate(text.toString(), Type.NICKNAME)
+            })
+
+        disposables.add(birth_year_edit.textChangeEvents()
+            .map{ it.text }
+            .subscribe{ text ->
+                validate(text.toString(), Type.YEAR)
+            })
+
+        disposables.add(viewModel.submit
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe{ submit ->
+                submit_button.isEnabled = submit
+            })
+
+        disposables.add(submit_button.clicks()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe{
+                Toast.makeText(this, "clicked!", Toast.LENGTH_SHORT).show()
+            })
     }
 
     private fun validate(text: String, type: Type){
         when(type){
             Type.EMAIL -> {
-                val pattern = android.util.Patterns.EMAIL_ADDRESS
-                if(pattern.matcher(text).matches()){
-                    Toast.makeText(this, text, Toast.LENGTH_LONG).show()
-                }
+                val emailPattern = android.util.Patterns.EMAIL_ADDRESS
+                if(emailPattern.matcher(text).matches()) viewModel.validateInput(type, true)
+                else viewModel.validateInput(type, false)
+            }
+
+            Type.PASSWORD -> {
+                val pwdPattern = Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}\$")
+                if(text.matches(pwdPattern)) viewModel.validateInput(type, true)
+                else viewModel.validateInput(type, false)
+            }
+
+            Type.CONFIRM -> {
+                if(text == password_edit.text.toString()) viewModel.validateInput(type, true)
+                else viewModel.validateInput(type, false)
+            }
+
+            Type.NICKNAME -> {
+                val nicknamePattern = Regex("^[\\w\\Wㄱ-ㅎㅏ-ㅣ가-힣]{8,30}\$")
+                if(text.matches(nicknamePattern)) viewModel.validateInput(type, true)
+                else viewModel.validateInput(type, false)
+            }
+
+            Type.YEAR -> {
+                if(text.length == 4) viewModel.validateInput(type, true)
+                else viewModel.validateInput(type, false)
             }
         }
-
-
     }
 }
 
--- app/src/main/java/com/example/signup/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/com/example/signup/ui/signup/SignUpViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.signup.ui.signup
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import io.reactivex.rxjava3.subjects.BehaviorSubject
 
@@ -13,4 +14,22 @@ class SignUpViewModel: ViewModel() {
     val nickname : BehaviorSubject<Boolean> = BehaviorSubject.createDefault(false)
 
     val birth: BehaviorSubject<Boolean> = BehaviorSubject.createDefault(false)
+
+    val submit: BehaviorSubject<Boolean> = BehaviorSubject.createDefault(false)
+
+    fun validateInput(type: Type, validated: Boolean){
+        when(type) {
+            Type.EMAIL -> email.onNext(validated)
+            Type.PASSWORD -> password.onNext(validated)
+            Type.CONFIRM -> confirmedPassword.onNext(validated)
+            Type.NICKNAME -> nickname.onNext(validated)
+            Type.YEAR -> birth.onNext(validated)
+        }
+        validateSubmit()
+    }
+
+    fun validateSubmit(){
+        if(email.value && password.value && confirmedPassword.value && nickname.value && birth.value) submit.onNext(true)
+        else submit.onNext(false)
+    }
 }
