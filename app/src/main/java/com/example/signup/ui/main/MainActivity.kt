a8c8ae9d21d30673b865bf8da810934724b9873e
gihop <giho-p@hanmail.net>
2020-09-27 00:07:23 +0900

    데이터 초기화 기능 구현.

--- app/src/main/java/com/example/signup/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/signup/ui/main/MainActivity.kt
@@ -1,21 +1,29 @@
 package com.example.signup.ui.main
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.signup.R
+import com.example.signup.data.UserInfo
 import com.example.signup.data.UserInfoDatabase
+import com.example.signup.data.UserInfoDatabaseDao
 import com.example.signup.ui.popup.ProgressBarPopupActivity
 import com.example.signup.ui.signup.SignUpActivity
 import com.jakewharton.rxbinding4.view.clicks
+import io.reactivex.Completable
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_failed_popup.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_progress_bar_popup.*
 
 class MainActivity : AppCompatActivity() {
-    private val disposable = CompositeDisposable()
+    private val disposables = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +45,7 @@ class MainActivity : AppCompatActivity() {
                 else user_optional_terms_text.text = "비동의"
             }
 
-        disposable.add(restart_button.clicks()
+        disposables.add(restart_button.clicks()
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe {
                 val restartIntent = Intent(this, SignUpActivity::class.java)
@@ -45,5 +53,25 @@ class MainActivity : AppCompatActivity() {
 
                 startActivity(restartIntent)
             })
+
+        disposables.add(data_reset_button.clicks()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                resetDatabase(database)
+                Toast.makeText(this, "데이터 초기화 완료", Toast.LENGTH_LONG).show()
+            })
+    }
+
+    fun resetDatabase(database: UserInfoDatabaseDao): io.reactivex.disposables.Disposable
+            = runOnIoScheduler { database.clear() }
+
+    fun runOnIoScheduler(func: () -> Unit): io.reactivex.disposables.Disposable
+            = Completable.fromCallable(func)
+        .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+        .subscribe()
+
+    override fun onStop() {
+        super.onStop()
+        disposables.clear()
     }
 }
